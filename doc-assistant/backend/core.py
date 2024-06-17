95cfb0bfd50cb2c8dcd4aa1126b4f5fd19073b72
k-i-r-a-n-m <mkiranmsg@gmail.com>
2024-06-17 11:55:40 +0530

    notebook - docAssistant added also .venv removed and pipenvFile added

--- doc-assistant/backend/core.py
+++ doc-assistant/backend/core.py
@@ -1,27 +1,44 @@
 import os
-from typing import Any
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI , OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain import hub
-from langchain.prompts import PromptTemplate
 
+from langchain import hub
+from dotenv import load_dotenv
+from typing import Any,List,Tuple
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.chains.retrieval import create_retrieval_chain
+from  langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
 
 load_dotenv()
-def run_llm(query:str) -> Any:
+def run_llm(query:str,chat_history:List[Tuple]=[]) -> Any:
+    # Initializing
     llm = ChatOpenAI(temperature=0)
     embedding = OpenAIEmbeddings()
     retrieval_prompt = hub.pull('langchain-ai/retrieval-qa-chat')
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
+
+    # Initializing VectorDB
     vector_store = PineconeVectorStore.from_existing_index(index_name=os.environ["INDEX_NAME"],embedding=embedding)
+
+    # Chains
     combine_docs_chain = create_stuff_documents_chain(llm,retrieval_prompt)
-    retrieval_chain = create_retrieval_chain(retriever=vector_store.as_retriever(),combine_docs_chain=combine_docs_chain)
-    result = retrieval_chain.invoke({"input": query})
-    # print(result['answer'])
+
+
+    chat_retriever_chain = create_history_aware_retriever(
+        llm, vector_store.as_retriever(), rephrase_prompt
+    )
+
+    # retrieval_chain = create_retrieval_chain(retriever=vector_store.as_retriever(),combine_docs_chain=combine_docs_chain)
+    retrieval_chain = create_retrieval_chain(chat_retriever_chain,
+                                             combine_docs_chain=combine_docs_chain)
+
+    result = retrieval_chain.invoke({"input": query, "chat_history": chat_history})
+
     return result
 
+
 if __name__ == "__main__":
-    run_llm("who is hitler?")
+    chat_history = [("human","who created langchain?"),("ai","harrison chase created it!")]
+    run_llm("tell me more about him", chat_history)
--- doc-assistant/main.py
+++ doc-assistant/main.py
@@ -10,6 +10,9 @@ if "user_prompt_history" not in st.session_state:
 if "chat_answer_history" not in st.session_state:
     st.session_state["chat_answer_history"] = []
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
 if __name__ == "__main__":
     st.header("Langchain(v1) -- Documentation Helper Bot")
     prompt = st.text_input("Prompt",placeholder="Enter your prompt here..")
@@ -18,10 +21,12 @@ if __name__ == "__main__":
         with st.spinner("Generating responses..."):
             # import time
             # time.sleep(3)
-            generated_response = run_llm(query=prompt)
+            generated_response = run_llm(query=prompt, chat_history = st.session_state["chat_history"])
             formatted_answer = f"\n{generated_response["answer"]}"
             st.session_state["user_prompt_history"].append(prompt)
             st.session_state["chat_answer_history"].append(formatted_answer)
+            for x in [("human", prompt), ("ai", formatted_answer)]:
+                st.session_state["chat_history"].append(x)
             print(generated_response)
 
     if st.session_state["chat_answer_history"]:
